1908c1cab5e8a1e4b1536cf099d58b6add556f53
harsh9503 <harshramoliya9503@gmail.com>
2024-01-30 18:18:04 +0530

    user and profile model added

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose= require("mongoose");
+
+require("dotenv").config();
+
+exports.dbconnect= () =>{
+        mongoose.connect(process.env.MONGODB_URL)
+        .then(()=>{console.log("DB CONNECTED SUCCESSFULLY")})
+        .catch((error)=>{
+            console.log("DB CONNECTION FAILED");
+            console.log(error);
+            process.exit(1);
+        })
+};
