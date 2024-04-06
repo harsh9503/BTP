3b070c6ecfdb7e01df2e6b891366d0f464e78995
Kadam-Darji <202001198@daiict.ac.in>
2024-04-06 14:28:32 +0530

    Commit before merge

--- backend/controllers/Course.js
+++ backend/controllers/Course.js
@@ -237,7 +237,13 @@ exports.getFullCourseDetails = async (req, res) => {
         },
       })
       .populate("category")
-      .populate("ratingAndReviews")
+      .populate({
+        path:"ratingAndReviews",
+        populate:{
+          path:"user",
+          select:"firstName lastName"
+        }
+      })
       .populate({
         path: "courseContent",
         populate: {
